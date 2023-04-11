7ac1feeafce52408a0776db5036d6248d54e4b92
SukhjotSaggu <78490392+SukhjotSaggu@users.noreply.github.com>
2023-04-11 17:50:06 -0400

    Doctor done

--- Doctor.py
+++ Doctor.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from firebase import db
 
 app = FastAPI()
 
@@ -10,43 +11,79 @@ class Doctor(BaseModel):
     age: int
     email: str
     pager_number: int
-    department: str
-
-
-doctors_db = [
-    Doctor(id=1, first_name='John', last_name='Doe', age=35, email='johndoe@example.com', pager_number=12345, department='Cardiology'),
-    Doctor(id=2, first_name='Jane', last_name='Smith', age=45, email='janesmith@example.com', pager_number=67890, department='Neurology'),
-]
-
+    department_id: str
 
 @app.get('/doctors')
 async def get_doctors():
+    doctors_db = {}
+    doctors_ref = db.collection(u'doctor')
+    docs = doctors_ref.stream()
+    for doc in docs:
+        doctors_db[doc.id] = doc.to_dict()
     return doctors_db
 
 @app.get('/doctors/{doctor_id}')
-async def get_doctor(doctor_id: int):
-    for doctor in doctors_db:
-        if doctor.id == doctor_id:
-            return doctor
-    return {'message': 'Doctor not found.'}
+async def get_doctor(doctor_id: str):
+    doctor = {}
+    doctor_ref = db.collection(u'doctor').document(doctor_id)
+    doctor_doc = doctor_ref.get()
+    if doctor_doc.exists:
+        doctor = doctor_doc.to_dict()  
+    else:
+        raise HTTPException(status_code=404, detail="Doctor not found")
+    return doctor
 
 @app.post('/doctors')
-async def create_doctor(doctor: Doctor):
-    doctors_db.append(doctor)
+async def create_doctor(d_first_name:str, d_last_name:str, d_email:str, d_age:int, d_pager_number:int, d_department_id:str):
+    new_doctor = {
+        u'doctor_id': 0,
+        u'first_name': d_first_name,
+        u'last_name': d_last_name,
+        u'email': d_email,
+        u'age': d_age,
+        u'pager_number': d_pager_number,
+        u'department_id': d_department_id
+    }
+
+    doctor_ref = db.collection(u'doctor').document()
+    new_doctor['doctor_id'] = doctor_ref.id
+    doctor_ref.set(new_doctor)
+
     return {'message': 'Doctor created.'}
 
 @app.put('/doctors/{doctor_id}')
-async def update_doctor(doctor_id: int, doctor: Doctor):
-    for i, d in enumerate(doctors_db):
-        if d.id == doctor_id:
-            doctors_db[i] = doctor
-            return {'message': 'Doctor updated.'}
-    return {'message': 'Doctor not found.'}
+async def update_doctor(doctor_id: str, d_first_name:str, d_last_name:str, d_email:str, d_age:int, d_pager_number:int, d_department_id:str):
+    doctor_ref = db.collection(u'doctor').document(doctor_id)
+    doctor_doc = doctor_ref.get()
+    if doctor_doc.exists:
+        if(d_first_name != "-1"):
+            doctor_ref.update({
+            u'first_name': d_first_name})
+        if(d_last_name != "-1"):
+            doctor_ref.update({
+            u'last_name': d_last_name})
+        if(d_email != "-1"):
+            doctor_ref.update({
+            u'email': d_email})
+        if(d_age != -1):
+            doctor_ref.update({
+            u'age': d_age})
+        if(d_pager_number != -1):
+            doctor_ref.update({
+            u'pager_number': d_pager_number})
+        if(d_department_id != "-1"):
+            doctor_ref.update({
+            u'department_id': d_department_id})
+    else:
+        raise HTTPException(status_code=404, detail="Doctor not found")
+    return {'message': 'Doctor Updated.'}
 
 @app.delete('/doctors/{doctor_id}')
-async def delete_doctor(doctor_id: int):
-    for i, doctor in enumerate(doctors_db):
-        if doctor.id == doctor_id:
-            doctors_db.pop(i)
-            return {'message': 'Doctor deleted.'}
-    return {'message': 'Doctor not found.'}
+async def delete_doctor(doctor_id: str):
+    doctor_ref = db.collection(u'doctor').document(doctor_id)
+    doctor_doc = doctor_ref.get()
+    if doctor_doc.exists:
+        doctor_ref.delete()
+    else:
+        raise HTTPException(status_code=404, detail="Doctor not found")
+    return {'message': 'Doctor Deleted.'}
